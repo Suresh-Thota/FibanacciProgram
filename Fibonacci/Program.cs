8ea76bb5e0af023c6f5be4e614a2003815d61c65
Suresh-Thota <Suresh.Thota@innroad.com>
2020-08-10 21:21:21 +0530

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        public static int Fib(int n)
+        {
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+            return Fib(n - 1) + Fib(n - 2);
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter a number ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" fib of given number is :" + Fib(n));
+        }
+    }
+}
